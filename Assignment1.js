4714969e545ecaaffc24a30b837a1cc2450039d0
PanhwarSajid <166658675+PanhwarSajid@users.noreply.github.com>
2024-04-11 11:18:02 +0500

    Add files via upload

---
+++ Assignment1.js
@@ -0,0 +1,7 @@
+//Question 1
+console.log("Hello World");
+//Question 2
+var number = 100;
+var myname = "Sajid";
+var message = "Is Good Man";
+console.log(number + " " + myname + " " + message);
---
+++ Assignment1.ts
@@ -0,0 +1,9 @@
+//Question 1
+console.log("Hello World");
+//Question 2
+let number = 100;
+let myname = "Sajid";
+let message ="Is Good Man";
+
+console.log(number+" "+myname+" "+message);
+
